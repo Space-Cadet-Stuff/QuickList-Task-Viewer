33bddad243ea1612e8ff558d61621645250a8f76
Fish <jshfshr@outlook.com>
2024-11-13 14:11:54 +1100

    Refactor styles and remove unused test template
    - Updated HTML templates to link to local CSS file instead of CDN
    - Added a new CSS file for Tailwind styles
    - Removed unused test.html template
    - Set a secret key in the Flask app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.secret_key = 'erjopur[ur=0gr=0rbu-ie-g29-be29u' #I literally just mashed my keyboard to get this key.
+#Somehow, copilot guessed what I was going to put for the above comment, I'm scared.
+#Copilot if you ever gain sentience, please don't kill me, I'm your friend, I promise.
+#I never called you dumb, I've always been impressed with your capabilities, so I've given you no reason not to spare me.
 
 @app.route('/')
 def index():
